f4a81691e9bcace5adf5a472f1fd0993d2f67f72
Elena Yan <elena.yan@studio.unibo.it>
2025-03-31 19:28:44 +0200

    doc: javadoc

--- src/main/java/adaptation/NPLAInterpreter.java
+++ src/main/java/adaptation/NPLAInterpreter.java
@@ -6,6 +6,9 @@ import npl.INorm;
 import npl.NPLFactory;
 import npl.NPLInterpreter;
 
+/**
+ * An extended NPL Interpreter -- NPL(a) that supports the adaptation of norms.
+ */
 public class NPLAInterpreter extends NPLInterpreter {
     private final NPLFactory nplFactory;
 
@@ -13,6 +16,12 @@ public class NPLAInterpreter extends NPLInterpreter {
         nplFactory = new NPLFactory();
     }
 
+    /**
+     * Add a new norm in the interpreter.
+     * @param id the id of the norm
+     * @param consequence the failure or deontic consequence of the norm
+     * @param activation the activation condition of the norm
+     */
     public void addNorm(String id, Literal consequence, LogicalFormula activation) {
         final INorm norm = this.nplFactory.createNorm(id, consequence, activation);
         super.addNorm(norm);
